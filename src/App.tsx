6afce95e1ed3791ed427835108fb2fbe407f263d
Pedro Santos <P3dro.santosc@gmail.com>
2024-02-19 21:44:12 -0300

    feat: ensure user go login page on click login button on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/home.page";
+import LoginPage from "./pages/login/login.page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
