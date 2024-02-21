81fa14feff241eb2ada43a2be794e03be832a0ab
Pedro Santos <P3dro.santosc@gmail.com>
2024-02-21 20:40:28 -0300

    feat: add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  startIcon?: React.ReactNode;
+}
+
+const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  startIcon,
+  ...rest
+}) => {
+  return (
+    <button
+      className="w-full bg-[#212529] text-white flex items-center justify-center rounded-xl shadow-lg border-none py-2 px-3 font-semibold transition-all duration-500 ease-in-out hover:bg-black cursor-pointer"
+      {...rest}
+    >
+      {startIcon && (
+        <div className="mr-2 h-full flex items-center">{startIcon}</div>
+      )}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
