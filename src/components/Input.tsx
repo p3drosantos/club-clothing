04f2923d5ec3d75d49130d49de367e691805c376
Pedro Santos <P3dro.santosc@gmail.com>
2024-02-23 20:00:50 -0300

    feat: add Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent, InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean;
+  errorMessage?: string;
+}
+
+const Input: FunctionComponent<InputProps> = React.forwardRef((props, ref) => {
+  return (
+    <div className="flex flex-col mb-5">
+      <input
+        className={`w-full bg-slate-100 px-5 py-3 shadow-md rounded-xl text-primaryGray ${
+          props.hasError
+            ? "border-2 border-red-500 focus:border-red-500 focus:outline-none placeholder-red-500"
+            : "focus:border-gray-400 border-2 placeholder-primaryGray focus:outline-none border-transparent"
+        }`}
+        {...props}
+        ref={ref as any}
+      />
+      {props.errorMessage && props.hasError && (
+        <span className="text-red-500 text-sm mt-1">{props.errorMessage}</span>
+      )}
+    </div>
+  );
+});
+
+Input.displayName = "Input";
+
+export default Input;
