520e532a42a97f52264ff5718c92ff3b47681c54
Pedro Santos <P3dro.santosc@gmail.com>
2024-02-23 20:02:17 -0300

    feat: ensure user go to login page on click button in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,28 @@
 import { BsCart3 } from "react-icons/bs";
 
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleHome = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="flex justify-between px-3 py-4 bg-[#212529] text-white items-center">
-      <div>
-        <h2 className="text-2xl font-bold">CLUB CLOTHING</h2>
+    <div className="flex justify-between px-3 py-4 bg-[#212529] text-white items-center ">
+      <div className="hover:cursor-pointer">
+        <h2 className="text-2xl font-bold" onClick={handleHome}>
+          CLUB CLOTHING
+        </h2>
       </div>
-      <div className="flex gap-6 font-medium">
+      <div className="flex gap-6 font-medium hover:cursor-pointer">
         <p>Explorar</p>
-        <p>Login</p>
+        <p onClick={handleLogin}>Login</p>
         <p>Criar Conta</p>
         <div className="flex gap-1">
           <BsCart3 size={24} /> 5
