4c24273b600ddd33745ab55cac4fd2cd0ea833da
Pedro Santos <P3dro.santosc@gmail.com>
2024-03-02 09:18:40 -0300

    feat: ensure that the user adds the product to the cart by clicking the ProductItem button

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,4 +1,11 @@
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
+
 import Product from "../types/product.types";
+
+import Button from "./Button";
+import { useContext } from "react";
+import { CartContext } from "../contexts/cartContext";
+
 interface ProductItemProps {
   product: Product;
 }
@@ -15,12 +22,28 @@ const ProductItem = ({ product }: ProductItemProps) => {
     // Definir altura como 100% h-[380px] w-[300px]
   };
 
+  const { addProduct } = useContext(CartContext);
+
+  const handleAddProduct = () => {
+    addProduct(product);
+  };
+
   return (
     <div className="flex flex-col">
       <div
-        className="rounded-[10px] shadow-custom"
+        className="rounded-[10px] shadow-custom relative"
         style={backgroundImageStyle}
-      ></div>
+      >
+        <div className="group absolute w-full  bg-black/60 rounded-[10px] opacity-0 hover:opacity-100 transition-opacity duration-1000"></div>
+        <div className="absolute w-full h-full bg-black/60 rounded-[10px] opacity-0 hover:opacity-100 p-5 flex flex-col justify-end transition-opacity duration-300">
+          <Button
+            startIcon={<MdOutlineShoppingCartCheckout />}
+            onClick={handleAddProduct}
+          >
+            Adicionar ao Carrinho
+          </Button>
+        </div>
+      </div>
       <div className="flex mt-1 justify-between">
         <p className="font-medium text-[1rem]">{product.name}</p>
         <p className="font-medium text-[1rem]">R${product.price}</p>
--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -1,17 +1,20 @@
 import { createContext, useState } from "react";
 
 import CartProduct from "../types/cart.types";
+import Product from "../types/product.types";
 
 interface ICartContext {
   products: CartProduct[];
   isOpen: boolean;
   toggleCart: () => void;
+  addProduct: (product: Product) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
   products: [],
   isOpen: false,
   toggleCart: () => {},
+  addProduct: () => {},
 });
 
 interface CartProviderProps {
@@ -20,14 +23,32 @@ interface CartProviderProps {
 
 const CartContextProvider = ({ children }: CartProviderProps) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [products] = useState<CartProduct[]>([]);
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
   const toggleCart = () => {
     setIsOpen((prevState) => !prevState);
   };
 
+  const addProduct = (product: Product) => {
+    setProducts((prevState) => {
+      const productAlreadyInCart = prevState.find(
+        (cartProduct) => cartProduct.id === product.id
+      );
+
+      if (productAlreadyInCart) {
+        return prevState.map((cartProduct) =>
+          cartProduct.id === product.id
+            ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+            : cartProduct
+        );
+      }
+
+      return [...prevState, { ...product, quantity: 1 }];
+    });
+  };
+
   return (
-    <CartContext.Provider value={{ products, isOpen, toggleCart }}>
+    <CartContext.Provider value={{ products, isOpen, toggleCart, addProduct }}>
       {children}
     </CartContext.Provider>
   );
