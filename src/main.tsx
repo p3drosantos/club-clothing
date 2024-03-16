1be22adb4ec392450d0a17d63b356152d7986940
Pedro Santos <P3dro.santosc@gmail.com>
2024-03-16 15:22:04 -0300

    chore: add redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.tsx";
+
+import { Provider } from "react-redux";
 
 import "./index.css";
 
 import UserContextProvider from "./contexts/userContext.tsx";
 import CategoryContextProvider from "./contexts/categoryContext.tsx";
 import CartContextProvider from "./contexts/cartContext.tsx";
+import store from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <CategoryContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </CategoryContextProvider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </Provider>
   </React.StrictMode>
 );
