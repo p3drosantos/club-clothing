8ec9bf491c7f081ddfb1e61912f1d80095920d16
Pedro Santos <P3dro.santosc@gmail.com>
2024-04-23 20:06:00 -0300

    feat: add responsiveness to CategoryOverview component

--- src/components/CategoryOverview.tsx
+++ src/components/CategoryOverview.tsx
@@ -9,8 +9,10 @@ const CategoryOverView = ({ category }: CategoryOverViewProps) => {
   return (
     <div>
       <div className="flex flex-col w-full mt-5">
-        <p className="font-bold text-xl mb-[8px]">{category.displayName}</p>
-        <div className="flex flex-wrap w-full justify-between">
+        <p className=" font-extrabold text-xl lg:mb-[10px] lg:items-start lg:justify-start flex items-center justify-center mb-[15px]">
+          {category.displayName}
+        </p>
+        <div className="flex flex-wrap w-full lg:justify-between items-center justify-center gap-10">
           {category.products.slice(0, 4).map((product) => (
             <ProductItem key={product.id} product={product} />
           ))}
